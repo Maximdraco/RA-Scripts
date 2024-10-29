bb9d18a77aac711caa194be77686959ed3b2c86e
Maximdraco <75223986+Maximdraco@users.noreply.github.com>
2024-10-29 09:49:17 -0300

    Added code for rich presence

--- Cruncheevos scripts/Amstrad CPC/3D Grand Prix.js
+++ Cruncheevos scripts/Amstrad CPC/3D Grand Prix.js
@@ -1,4 +1,4 @@
-import { AchievementSet, define as $ } from '@cruncheevos/core'
+import { AchievementSet, define as $ , RichPresence} from '@cruncheevos/core'
 
 const set = new AchievementSet({ gameId: 20319, title: '3D Grand Prix' })
 
@@ -6,11 +6,11 @@ const set = new AchievementSet({ gameId: 20319, title: '3D Grand Prix' })
 // Variables //
 ///////////////
 
-const cities = ["Mosport", "Zandour", "Silverstone",
-                "Anderstorp", "Jarama", "Roeur",
+const cities = ["Mosport", "Zandvoort", "Silverstone",
+                "Anderstorp", "Jarama", "Rouen",
                 "Brands Hatch", "Kyahami"]
 
-const countries = ["Canada", "Belgium", "England",
+const countries = ["Canada", "Netherlands", "England",
                    "Sweden", "Spain", "France",
                    "Kent", "South Africa"]
 
@@ -21,7 +21,9 @@ const Grand_Prix_Amsoft_1985 = {
   "Course": 0xa51e,
   "Position": 0xa510,
   "Screen": 0xc54e,
-  "You_win_screen": 0xb8ab
+  "You_win_screen": 0xb8ab,
+  "Score": 0xa51f,
+  "Lap": 0xa50f
 }
 
 const versions = [Grand_Prix_Amsoft_1985]
@@ -67,13 +69,17 @@ function not_title_screen(version){
   )
 }
 
+// Function to work with multiple logic
+// Have to be edited per set and achievement
+// This is mostly for practice, I don't think there are other
+// versions of this game
 function multiple_versions_logic(versions, pos, track){
   var logic = {}
   if (versions.length > 1){
-    var logic = {}
-    logic["core"] = $(["", "Value", "8bit", 1, "=", "Value", "", 1])
+    logic["core"] = $(["", "Value", "", 1, "=", "Value", "", 1])
     for (let i = 0; i < versions.length; i++){
-      logic["alt${i+1}"] = $(
+      var n = "alt" + (i+1)
+      logic[n] = $(
         course(versions[i], track),
         position(versions[i], pos),
         not_title_screen(versions[i]),
@@ -93,6 +99,67 @@ function multiple_versions_logic(versions, pos, track){
   return(logic)
 }
 
+function leaderderboard_start(versions){
+  var logic = {}
+  if (versions.length > 1){
+    logic["core"] = $(["", "Value", "", 1, "=", "Value", "", 1])
+    for (let i = 0; i < versions.length; i++){
+      var n = "alt" + (i+1)
+      logic[n] = $(['', 'Mem', '24bit', versions[i]["Score"], '!=', 'Value', '', 0])
+    }
+  }
+  else{
+    logic = $(['', 'Mem', '24bit', versions[0]["Score"], '!=', 'Value', '', 0])
+  }
+  return(logic)
+}
+
+function leaderderboard_cancel(versions){
+  var logic = {}
+  if (versions.length > 1){
+    logic["core"] = $(["", "Value", "", 1, "=", "Value", "", 1])
+    for (let i = 0; i < versions.length; i++){
+      var n = "alt" + (i+1)
+      logic[n] = $(['', 'Mem', '8bit', versions[i]["Screen"], '=', 'Value', '', 207])
+    }
+  }
+  else{
+    logic = $(['', 'Mem', '8bit', versions[0]["Screen"], '=', 'Value', '', 207])
+  }
+  return(logic)
+}
+
+function leaderderboard_submit(versions){
+  var logic = {}
+  if (versions.length > 1){
+    logic["core"] = $(["", "Value", "", 1, "=", "Value", "", 1])
+    for (let i = 0; i < versions.length; i++){
+      var n = "alt" + (i+1)
+      logic[n] = $(
+      ['', 'Mem', '8bit', versions[i]["Course"], '=', 'Value', '', 0],
+      ['', 'Mem', '8bit', versions[i]["Screen"], '=', 'Value', '', 39],
+      ['', 'Mem', '8bit', versions[i]["Lap"], '=', 'Value', '', 3],
+      ['', 'Mem', '8bit', versions[i]["Position"], '<=', 'Value', '', 3],
+      ['', 'Mem', '8bit', versions[i]["You_win_screen"], '=', 'Value', '', 183]
+      )
+    }
+  }
+  else{
+    logic = $(
+      ['', 'Mem', '8bit', versions[0]["Course"], '=', 'Value', '', 0],
+      ['', 'Mem', '8bit', versions[0]["Screen"], '=', 'Value', '', 39],
+      ['', 'Mem', '8bit', versions[0]["Lap"], '=', 'Value', '', 3],
+      ['', 'Mem', '8bit', versions[0]["Position"], '<=', 'Value', '', 3],
+      ['', 'Mem', '8bit', versions[0]["You_win_screen"], '=', 'Value', '', 183]
+    )
+  }
+  return(logic)
+}
+
+// Rework this is another version of the game exist
+function leaderboard_value(versions){
+    return($(['Measured', 'BCD', '24bit', versions[0]["Score"]]))
+}
 ///////////////
 // Main code //
 ///////////////
@@ -114,9 +181,7 @@ for (let i = 0; i < cities.length; i++){
     description: "End in the Top 3 in the " + cities[i] + " Track.",
     points: p,
     type: t,
-    conditions: $(
-      multiple_versions_logic(versions, 3, i)
-    )
+    conditions: multiple_versions_logic(versions, 3, i)
     })
 
   p = 25
@@ -128,9 +193,7 @@ for (let i = 0; i < cities.length; i++){
     description: "End in first place in the " + cities[i] + " Track.",
     points: p,
     type: "",
-    conditions: $(
-        multiple_versions_logic(versions, 1, i)
-    )
+    conditions: multiple_versions_logic(versions, 1, i)
   })
 }
 
@@ -141,18 +204,43 @@ set.addLeaderboard({
   lowerIsBetter: false,
   type: 'VALUE',
   conditions: {
-    start: $(['', 'Mem', '24bit', 0xa51f, '!=', 'Value', '', 0]),
-    cancel: $(['', 'Mem', '8bit', 0xc54e, '=', 'Value', '', 207]),
-    submit: $(
-      ['', 'Mem', '8bit', 0xa51e, '=', 'Value', '', 0],
-      ['', 'Mem', '8bit', 0xc54e, '=', 'Value', '', 39],
-      ['', 'Mem', '8bit', 0xa50f, '=', 'Value', '', 3],
-      ['', 'Mem', '8bit', 0xa510, '<=', 'Value', '', 3],
-      ['', 'Mem', '8bit', 0xb8ab, '=', 'Value', '', 183],
-    ),
-    value: $(['Measured', 'BCD', '24bit', 0xa51f]),
-  },
-  id: 52853,
+    start: leaderderboard_start(versions),
+    cancel: leaderderboard_cancel(versions),
+    submit: leaderderboard_submit(versions),
+    value: leaderboard_value(versions),
+  }
 })
+
+export const rich = RichPresence({
+  lookupDefaultParameters: { keyFormat: 'dec', compressRanges: true },
+  // Wraps calls to RichPresence.format
+  format: {
+    Score: 'VALUE',
+  },
+  // Wraps calls to RichPresence.lookup
+  lookup: {
+    Track: {
+      // No need to specify name, it's taken from object
+      values: {
+        '*': 'This message shouldn\'t appear',
+        0: "Mosport",
+        1: "Zandvoort",
+        2: "Silverstone",
+        3: "Anderstorp",
+        4: "Jarama",
+        5: "Rouen",
+        6: "Brands Hatch",
+        7: "Kyahami"
+      },
+    },
+  },
+  displays: ({ lookup, format, tag }) => [
+    [
+      $(['', 'Mem', '8bit', versions[0]["Screen"], '=', 'Value', '', 207]),
+      tag `In the title screen`,
+    ],
+    `Racing in ${lookup.Track.at('0xha51e')}`,
+  ],
+ })
 
 export default set
